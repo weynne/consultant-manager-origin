a5cd3dd881faafa4a9e7accc1c71fee256e21ea4
Weynne <weynne.lima@gmail.com>
2024-04-20 12:57:00 -0300

    H2 database, test profile, JPA

--- src/main/java/br/com/brencorp/consman/entities/Estado.java
+++ src/main/java/br/com/brencorp/consman/entities/Estado.java
@@ -3,9 +3,19 @@ package br.com.brencorp.consman.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "estado")
 public class Estado implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String uf;
 
