bfae8855e8de6211c617cf449071012af4058ab1
Weynne <weynne.lima@gmail.com>
2024-04-20 13:33:50 -0300

    Service layer, component registration

--- src/main/java/br/com/brencorp/consman/resources/EstadoResource.java
+++ src/main/java/br/com/brencorp/consman/resources/EstadoResource.java
@@ -1,19 +1,35 @@
 package br.com.brencorp.consman.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.brencorp.consman.entities.Estado;
+import br.com.brencorp.consman.services.EstadoService;
 
 @RestController
 @RequestMapping(value = "/estados")
 public class EstadoResource {
 
+	@Autowired
+	private EstadoService service;
+
 	@GetMapping
-	public ResponseEntity<Estado> findAll() {
-		Estado e = new Estado(1L, "PE");
-		return ResponseEntity.ok().body(e);
+	public ResponseEntity<List<Estado>> findAll() {
+		List<Estado> list = service.findAll();
+
+		return ResponseEntity.ok().body(list);
 	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Estado> findById(@PathVariable Long id) {
+		Estado obj = service.findById(id);
+		return ResponseEntity.ok(obj);
+	}
+
 }
---
+++ src/main/java/br/com/brencorp/consman/services/EstadoService.java
@@ -0,0 +1,26 @@
+package br.com.brencorp.consman.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.brencorp.consman.entities.Estado;
+import br.com.brencorp.consman.repositories.EstadoRepository;
+
+@Service
+public class EstadoService {
+	
+	@Autowired
+	private EstadoRepository repository;
+
+	public List<Estado> findAll(){
+		return repository.findAll();
+	}
+	
+	public Estado findById(Long id) {
+		Optional <Estado> obj = repository.findById(id);
+		return obj.get();
+	}
+}
