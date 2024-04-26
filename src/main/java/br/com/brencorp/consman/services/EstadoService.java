478cd9662b078ce82691cdc5446e497734bc3e54
Weynne <weynne.lima@gmail.com>
2024-04-26 18:30:38 -0300

    Exception handling - findById

--- src/main/java/br/com/brencorp/consman/services/EstadoService.java
+++ src/main/java/br/com/brencorp/consman/services/EstadoService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.brencorp.consman.entities.Estado;
 import br.com.brencorp.consman.repositories.EstadoRepository;
+import br.com.brencorp.consman.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class EstadoService {
@@ -21,17 +22,17 @@ public class EstadoService {
 
 	public Estado findById(Long id) {
 		Optional<Estado> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public Estado insert(Estado obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
 	public Estado update(Long id, Estado obj) {
 		Estado entity = repository.getReferenceById(id);
 		updateData(entity, obj);
